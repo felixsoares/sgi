b877be20df81dadd7fc55cb33f1166f9791c532b
felixsoares <felix.soares.ferreira@gmail.com>
2017-03-16 23:12:13 -0300

    alteraçoes

--- build/generated-sources/ap-source-output/br/com/sgi/model/Categoria_.java
+++ build/generated-sources/ap-source-output/br/com/sgi/model/Categoria_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-02-09T23:20:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-05T20:50:28")
 @StaticMetamodel(Categoria.class)
 public class Categoria_ { 
 
--- build/generated-sources/ap-source-output/br/com/sgi/model/Lancamento_.java
+++ build/generated-sources/ap-source-output/br/com/sgi/model/Lancamento_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-02-09T23:20:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-05T20:50:28")
 @StaticMetamodel(Lancamento.class)
 public class Lancamento_ { 
 
