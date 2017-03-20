f00533ae788de96f5f9a96c9f5abcd8b495ccf5a
felixsoares <felix.soares.ferreira@gmail.com>
2017-03-19 22:45:56 -0300

    alterações

--- build/generated-sources/ap-source-output/br/com/sgi/model/Membro_.java
+++ build/generated-sources/ap-source-output/br/com/sgi/model/Membro_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-19T22:41:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-19T22:45:29")
 @StaticMetamodel(Membro.class)
 public class Membro_ { 
 
